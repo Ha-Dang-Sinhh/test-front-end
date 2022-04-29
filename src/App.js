10fcd6c6d0b7bdbfb0b66859212edaaa0aad950b
Sinh <hadangsinh1911@gmail.com>
2022-04-29 15:51:52 +0700

    add code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Category from './pages/Category'
+
+function App() {
+  return (
+    <div className="App">
+      <Category />
+    </div>
+  );
+}
+
+export default App;
